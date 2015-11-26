c39c2e9ccd690eea99d5721ac73f3238c41b8280
Gokhan Derala <gderala@gmail.com>
2015-11-26 12:50:04 +0100

    created controller and index view

---
+++ app/controllers/coctails_controller.rb
@@ -0,0 +1,2 @@
+class CoctailsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'conctails#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
